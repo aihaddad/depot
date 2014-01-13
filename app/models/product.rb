db5febfbadf4b556fca0642550da8eb0eaa4263a
aihaddad <eng_ai@msn.com>
2014-01-13 12:53:29 +0000

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(png|gif|jpg)\Z}i,
+    message: 'must be a URL for an image of GIF, PNG or JPG format'
+  }
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
